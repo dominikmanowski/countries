1d7cc40a2a1494aaefd010eece3ade81baf6e52e
Dominik Manowski <manowski@protonmail.com>
2021-01-17 22:04:33 +0100

    Add Country and Countries intefraces

--- src/useGQLQuery.ts
+++ src/useGQLQuery.ts
@@ -1,10 +1,19 @@
 import { useQuery } from 'react-query'
 import { request } from 'graphql-request'
 
+interface Country {
+  name: string
+  code: string
+}
+
+interface Countries {
+  countries: Country[]
+}
+
 export default (key: string, query: any) => {
   const endpoint = 'https://countries.trevorblades.com/'
 
   const fetchData = async () => request(endpoint, query)
 
-  return useQuery(key, fetchData)
+  return useQuery<Countries, Error>(key, fetchData)
 }
