b863c267ebda7082daa8b2da8caafe813f875f75
Dominik Manowski <manowski@protonmail.com>
2021-01-17 19:03:54 +0100

    Setup QueryClient

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
   document.getElementById('root'),
 )
