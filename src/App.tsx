2bed4e245e0a61152b8d2bc3b29ffe5cde0f57b9
Dominik Manowski <manowski@protonmail.com>
2021-01-17 20:08:02 +0100

    Display coutry list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import gql from 'graphql-tag'
+import useGQLQuery from './useGQLQuery'
+
+const GET_COUNTRIES = gql`
+  query {
+    countries {
+      code
+      name
+    }
+  }
+`
+
 function App() {
+  const { data, isLoading, error } = useGQLQuery('countries', GET_COUNTRIES)
+  if (error) return <p>An error ocurred</p>
+
   return (
     <div className='App'>
-      <h1>Countries</h1>
+      <h1>Countries:</h1>
+      {isLoading && <p>Loading...</p>}
+      <ul>
+        {data?.countries.map(({ name, code }) => (
+          <li key={code}>{name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
